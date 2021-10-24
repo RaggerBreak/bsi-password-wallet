021b287be7709abca4c93f498005b261c8cbf53b
RaggerLtp <ragger.break@gmail.com>
2021-10-24 13:31:04 +0200

    init be

---
+++ bsi-password-wallet-be/src/main/java/com/raggerbreak/bsipasswordwalletbe/BsiPasswordWalletBeApplication.java
@@ -0,0 +1,13 @@
+package com.raggerbreak.bsipasswordwalletbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsiPasswordWalletBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BsiPasswordWalletBeApplication.class, args);
+    }
+
+}
---
+++ bsi-password-wallet-be/src/test/java/com/raggerbreak/bsipasswordwalletbe/BsiPasswordWalletBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raggerbreak.bsipasswordwalletbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsiPasswordWalletBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
