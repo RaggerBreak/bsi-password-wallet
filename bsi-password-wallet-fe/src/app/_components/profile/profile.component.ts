5d57867811135f114d4de4f2570df445f19ea8cd
RaggerLtp <ragger.break@gmail.com>
2021-10-27 20:23:09 +0200

    change password

--- bsi-password-wallet-fe/src/app/_components/profile/profile.component.ts
+++ bsi-password-wallet-fe/src/app/_components/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {TokenStorageService} from "../../_services/token-storage.service";
+import {UserService} from "../../_services/user.service";
 
 @Component({
   selector: 'app-profile',
@@ -8,12 +9,28 @@ import {TokenStorageService} from "../../_services/token-storage.service";
 })
 export class ProfileComponent implements OnInit {
 
+  formChangePassword: any = {
+    oldPassword: null,
+    newPassword: null,
+    passwordForm: null
+  };
+  passwordForms: string[] = ['SHA512', 'HMAC'];
+
   currentUser: any;
 
-  constructor(private token: TokenStorageService) { }
+  constructor(private token: TokenStorageService, private userService: UserService) { }
 
   ngOnInit(): void {
     this.currentUser = this.token.getUser();
+    this.formChangePassword.oldPassword = null;
+    this.formChangePassword.newPassword = null;
+    this.formChangePassword.passwordForm = null;
   }
 
+  onClickChangePassword() {
+    const { oldPassword, newPassword, passwordForm } = this.formChangePassword;
+    this.userService.changePassword(oldPassword, newPassword, passwordForm).subscribe(data => {
+      this.ngOnInit();
+    });
+  }
 }
--- bsi-password-wallet-fe/src/app/_services/user.service.ts
+++ bsi-password-wallet-fe/src/app/_services/user.service.ts
@@ -1,26 +1,41 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
 
 
 const API_URL = 'http://localhost:8080/api/board/';
+
+const API_URL_USER = 'http://localhost:8080/api/user/';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
   getPublicContent(): Observable<any> {
-    return this.http.get(API_URL + 'all', {responseType: 'text'});
+    return this.httpClient.get(API_URL + 'all', {responseType: 'text'});
   }
 
   getUserBoard(): Observable<any> {
-    return this.http.get(API_URL + 'user', {responseType: 'text'})
+    return this.httpClient.get(API_URL + 'user', {responseType: 'text'})
   }
 
   getAdminBoard(): Observable<any> {
-    return this.http.get(API_URL + 'admin', { responseType: 'text' });
+    return this.httpClient.get(API_URL + 'admin', { responseType: 'text' });
   }
+
+  changePassword(oldPassword: string, newPassword: string, passwordForm: string): Observable<any> {
+    return this.httpClient.post(API_URL_USER + 'password/change', {
+      oldPassword,
+      newPassword,
+      passwordForm
+    }, httpOptions);
+  }
+
 }
